986552d3f54948ebf391bdf8dbfd739df78d9df9
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-23 19:18:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,8 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    doc = Nokogiri(HTML)(open())
+    doc = Nokogiri(HTML)(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
   
   def print_courses
@@ -22,6 +23,8 @@ class Scraper
   end
   
 end
+
+Scraper.new.get_page
 
 
 
